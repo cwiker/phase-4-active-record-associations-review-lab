2881f9dce1ef03a903c4f3aa84acebcc8ed34ba7
Chris Wiker <cwiker7@gmail.com>
2022-02-04 12:15:36 -0500

    commit tree 07a165c9b8ab4fd3ecd7192e823bf99cd68b8b1c

--- db/migrate/20210506153111_create_taxis.rb
+++ db/migrate/20210506153111_create_taxis.rb
@@ -1,6 +1,9 @@
 class CreateTaxis < ActiveRecord::Migration[6.1]
   def change
     create_table :taxis do |t|
+      t.integer :passenger_id
+      t.integer :ride_id
+
 
       t.timestamps
     end
